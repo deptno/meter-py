e98381187a79fdad4c4e3116c05124e052993e0a
bglee <deptno@gmail.com>
2017-02-06 13:03:10 +0900

    meterRound, pyRound

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,14 @@
-import {py, meter} from './index';
-import '@types/jest';
+import {py, meter, pyRound, meterRound} from './index';
 
 test('1 py to 3.30579㎥', () => {
     expect(py(1)).toEqual(3.30579);
 });
 test('1㎥ to 0.3025py', () => {
     expect(meter(1)).toEqual(0.3025);
+});
+test('1 py to 3㎥', () => {
+    expect(pyRound(1)).toEqual(3);
+});
+test('3.3㎥ to 1py', () => {
+    expect(meterRound(3.3)).toEqual(1);
 });
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,17 @@ const PY_1 = 1.000001475;
 export function py(meter) {
     return meter * 3.30579;
 }
+export function pyRound(meter) {
+    return Math.round(py(meter));
+}
 export function squareMeter(py) {
     if (py.toString().includes('.')) {
         return py * 0.3025;
     }
     return Number((py * PY_1 * 0.3025).toFixed(4));
 }
+export function squareMeterRound(py) {
+    return Math.round(squareMeter(py));
+}
 export const meter = squareMeter;
+export const meterRound = squareMeterRound;
