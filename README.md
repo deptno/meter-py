8fbdb5db8f5bd7c879527f3b48ef0be3b4bf2d4a
bglee <deptno@gmail.com>
2017-02-27 14:43:50 +0900

    swap api
    py returns py
    meter returns meter
    
    add readme

---
+++ README.md
@@ -0,0 +1,13 @@
+meter-py
+===
+[![Build Status](https://travis-ci.org/deptno/meter-py.svg?branch=master)](https://travis-ci.org/deptno/meter-py)
+
+## API
+
+### py(meter: number): PY
+### pyRound(meter: number): PY
+### meter(meter: number): METER
+### meterRound(meter: number): METER
+
+## LICENSE
+MIT
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 const PY_1 = 1.000001475;
 export function py(meter) {
-    return meter * 3.30579;
+    if (meter.toString().includes('.')) {
+        return meter * 0.3025;
+    }
+    return Number((meter * PY_1 * 0.3025).toFixed(4));
 }
 export function pyRound(meter) {
     return Math.round(py(meter));
 }
 export function squareMeter(py) {
-    if (py.toString().includes('.')) {
-        return py * 0.3025;
-    }
-    return Number((py * PY_1 * 0.3025).toFixed(4));
+    return py * 3.30579;
 }
 export function squareMeterRound(py) {
     return Math.round(squareMeter(py));
